fc522334722ee8d578fbf43b99d6a1d949f2d56d
Jeongsohee <toy3045@gmail.com>
2024-07-22 02:23:25 +0900

    ✅ Test: #6 로그인, 회원가입 테스트 추가

--- tests/test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Dashboard from '../src/pages/Dashboard';
-
-test('renders Home Page text', () => {
-  render(<Dashboard />);
-  const element = screen.getByText(/Home Page/i);
-  expect(element).toBeInTheDocument();
-});
