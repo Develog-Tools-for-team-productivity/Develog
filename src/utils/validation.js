46689fed4448792ff12a0acb3b4a49d3baf6521e
Jeongsohee <toy3045@gmail.com>
2024-07-29 22:35:49 +0900

    🔨 Remove: #7 validation 파일 제거
    - github Oauth 방식으로 로그인 재구현하여 파일 제거

--- src/utils/validation.js
+++
@@ -1,8 +0,0 @@
-export const isEmailValid = email => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-};
-
-export const isPasswordStrong = password => {
-  return password.length >= 8;
-};
