6b4229451f95d4d60bd6aeac6927d82b9cf54853
Jeongsohee <toy3045@gmail.com>
2024-08-07 15:52:23 +0900

    ✨ Feat: #8 CommonList 컴포넌트 구현
    - 데이터와 헤더를 사용하여 표 형태로 렌더링하는 컴포넌트 추가

--- src/components/list/CommonList.jsx
+++ src/components/list/CommonList.jsx
@@ -1,21 +1,47 @@
 import ListStateCard from '../../components/stateCard/ListStateCard';
 import ListPieChartCard from '../../components/stateCard/ListPieChartCard';
+import { getColorByKey } from '../../utils/colorUtils';
+
 import styles from './list.module.css';
 
-const CommonList = ({ headers, data, page, setState }) => {
-  const dotColumns = [
-    'cycleTime',
-    'codingTime',
-    'pickUp',
-    'review',
-    'deploy',
-    'commits',
-    'prSize',
-  ];
+const DOT_COLUMNS = [
+  'cycleTime',
+  'codingTime',
+  'pickUp',
+  'review',
+  'deploy',
+  'prSize',
+];
 
-  const openProjectList = () => {
+const CommonList = ({ headers, data, page, setState }) => {
+  const handleRowClick = index => {
     if (page === 'ProjectDeliveryTracker') {
-      setState(false);
+      setState(index);
+    }
+  };
+
+  const renderCell = (key, value) => {
+    if (key === 'activePeople' || key === 'delivery') {
+      return (
+        <ListStateCard
+          icon={value.icon}
+          value={value.value}
+          label={value.label}
+        />
+      );
+    } else if (key === 'investmentProfile') {
+      return <ListPieChartCard items={value} />;
+    } else {
+      return (
+        <>
+          {value}
+          {DOT_COLUMNS.includes(key) && (
+            <span
+              className={`${styles.mark} ${styles[getColorByKey(key, value)]}`}
+            ></span>
+          )}
+        </>
+      );
     }
   };
 
@@ -33,27 +59,13 @@ const CommonList = ({ headers, data, page, setState }) => {
           {data.map((item, index) => (
             <tr
               key={index}
-              onClick={openProjectList}
-              className={page === 'ProjectDeliveryTracker' && 'pointer'}
+              onClick={() => handleRowClick(index)}
+              className={
+                page === 'ProjectDeliveryTracker' ? styles.pointer : ''
+              }
             >
               {Object.entries(item).map(([key, value], i) => (
-                <td key={i}>
-                  {key === 'activePeople' || key === 'delivery' ? (
-                    <ListStateCard
-                      key={index}
-                      icon={value.icon}
-                      value={value.value}
-                      label={value.label}
-                    />
-                  ) : key === 'investmentProfile' ? (
-                    <ListPieChartCard items={value} />
-                  ) : (
-                    value
-                  )}
-                  {dotColumns.includes(key) && (
-                    <span className={styles.redDot}></span>
-                  )}
-                </td>
+                <td key={i}>{renderCell(key, value)}</td>
               ))}
             </tr>
           ))}
---
+++ src/constants/cycleTimeConstants.js
@@ -0,0 +1,12 @@
+export const HEADERS = [
+  'Pull Request',
+  '작성자',
+  'Repositories',
+  '개발 작업 주기',
+  '코드작업 시간',
+  'PR 대기시간',
+  '코드리뷰',
+  '배포',
+  'Commits',
+  'PR Size',
+];
