3ea6792924fdebcacd5231f5b8f623a750a32a6a
Jeongsohee <toy3045@gmail.com>
2024-08-07 15:50:18 +0900

    ✨ Feat: #8 CycleTime 컴포넌트 구현
    - useState 및 useAtom을 사용하여 주기 시간 데이터, 확장된 통계, 날짜 범위, 선택된 레포지토리 등을 상태로 관리
    - useFetchCycleTimeData를 통한 주기 시간 데이터 페칭
    - Header, TeamViewHeader, StateCard, MainBoard, CommonList 컴포넌트를 사용하여 UI 구성

--- src/pages/cycleTime/CycleTime.jsx
+++ src/pages/cycleTime/CycleTime.jsx
@@ -1,163 +1,111 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useAtom } from 'jotai';
+import { dateRangeAtom } from '../../stores/useStore';
 import Header from '../../components/header/Header';
 import TeamViewHeader from '../../components/header/TeamViewHeader';
 import StateCard from '../../components/stateCard/StateCard';
 import MainBoard from '../../components/board/MainBoard';
 import CommonList from '../../components/list/CommonList';
+import { useFetchCycleTimeData } from '../../utils/useFetchCycleTimeData';
+import { HEADERS } from '../../constants/cycleTimeConstants';
 
 import styles from '../../components/stateCard/stateCard.module.css';
 
 const CycleTime = () => {
-  const stats = [
-    {
-      icon: 'cycleTime',
-      value: '12d 1h',
-      label: 'Cycle Time',
-    },
-  ];
+  const [stats, setStats] = useState({});
+  const [extendedStats, setExtendedStats] = useState({});
+  const [cycleTimeListData, setCycleTimeListData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [selectedRepo, setSelectedRepo] = useState('average');
+  const [selectedRepositories, setSelectedRepositories] = useState([]);
+  const [dateRange, setDateRange] = useAtom(dateRangeAtom);
+  const [error, setError] = useState(null);
 
-  const extendedStats = {
-    cycleTime: {
-      items: [
-        { value: '16 hour', label: 'Coding' },
-        { value: '2 days', label: 'PickUp' },
-        { value: '7 hour', label: 'Review' },
-        { value: '90 min', label: 'Deploy' },
-      ],
-    },
+  const location = useLocation();
+  const { fetchCycleTimeData } = useFetchCycleTimeData();
+
+  useEffect(() => {
+    const endDate = new Date();
+    const startDate = new Date(endDate);
+    startDate.setDate(startDate.getDate() - 10);
+
+    setDateRange({
+      startDate: startDate.toISOString().split('T')[0],
+      endDate: endDate.toISOString().split('T')[0],
+    });
+  }, [location]);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const data = await fetchCycleTimeData(
+        dateRange.startDate,
+        dateRange.endDate
+      );
+      setStats(data.stats[1]);
+      setExtendedStats(data.extendedStats);
+      setCycleTimeListData(data.cycleTimeListData);
+      setSelectedRepositories(data.selectedRepositories);
+      filterData(data.cycleTimeListData, selectedRepo);
+      setError(null);
+    } catch (err) {
+      console.error('Error fetching data:', err);
+      setError('데이터를 가져오는 중 오류가 발생했습니다.');
+    }
+  }, [dateRange, selectedRepo, fetchCycleTimeData]);
+
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      fetchData();
+    }
+  }, [dateRange, fetchData]);
+
+  const handleDateRangeChange = (startDate, endDate) => {
+    setDateRange({ startDate, endDate });
   };
 
-  const headers = [
-    'Pull Request',
-    'Contributors',
-    'Repositories',
-    'Cycle Time',
-    'Coding Time',
-    'PickUp',
-    'Review',
-    'Deploy',
-    'Commits',
-    'PR Size',
-  ];
+  const handleRepoSelect = repoId => {
+    setSelectedRepo(repoId);
+    filterData(cycleTimeListData, repoId);
+  };
 
-  const cycleTimeData = [
-    {
-      pullRequest: 'Feat#119 save pr actual size along stats',
-      contributors: 'origami',
-      repositories: 'origami',
-      cycleTime: '12d',
-      codingTime: '4d',
-      pickUp: '21h',
-      review: '3d',
-      deploy: '1h',
-      commits: 54,
-      prSize: 81,
-    },
-    {
-      pullRequest: 'Feat#119 save pr actual size along stats',
-      contributors: 'origami',
-      repositories: 'origami',
-      cycleTime: '12d',
-      codingTime: '4d',
-      pickUp: '21h',
-      review: '3d',
-      deploy: '1h',
-      commits: 54,
-      prSize: 81,
-    },
-    {
-      pullRequest: 'Feat#119 save pr actual size along stats',
-      contributors: 'origami',
-      repositories: 'origami',
-      cycleTime: '12d',
-      codingTime: '4d',
-      pickUp: '21h',
-      review: '3d',
-      deploy: '1h',
-      commits: 54,
-      prSize: 81,
-    },
-    {
-      pullRequest: 'Feat#119 save pr actual size along stats',
-      contributors: 'origami',
-      repositories: 'origami',
-      cycleTime: '12d',
-      codingTime: '4d',
-      pickUp: '21h',
-      review: '3d',
-      deploy: '1h',
-      commits: 54,
-      prSize: 81,
-    },
-    {
-      pullRequest: 'Feat#119 save pr actual size along stats',
-      contributors: 'origami',
-      repositories: 'origami',
-      cycleTime: '12d',
-      codingTime: '4d',
-      pickUp: '21h',
-      review: '3d',
-      deploy: '1h',
-      commits: 54,
-      prSize: 81,
-    },
-    {
-      pullRequest: 'Feat#119 save pr actual size along stats',
-      contributors: 'origami',
-      repositories: 'origami',
-      cycleTime: '12d',
-      codingTime: '4d',
-      pickUp: '21h',
-      review: '3d',
-      deploy: '1h',
-      commits: 54,
-      prSize: 81,
-    },
-    {
-      pullRequest: 'Feat#119 save pr actual size along stats',
-      contributors: 'origami',
-      repositories: 'origami',
-      cycleTime: '12d',
-      codingTime: '4d',
-      pickUp: '21h',
-      review: '3d',
-      deploy: '1h',
-      commits: 54,
-      prSize: 81,
-    },
-    {
-      pullRequest: 'Feat#119 save pr actual size along stats',
-      contributors: 'origami',
-      repositories: 'origami',
-      cycleTime: '12d',
-      codingTime: '4d',
-      pickUp: '21h',
-      review: '3d',
-      deploy: '1h',
-      commits: 54,
-      prSize: 81,
-    },
-  ];
+  const filterData = (data, repoId) => {
+    const filtered =
+      repoId === 'average'
+        ? data
+        : data.filter(item => item.repositories === repoId);
+    setFilteredData(filtered);
+  };
 
   return (
     <>
       <Header headerText="Cycle Time" />
-      <div
-        className={`${styles.teamViewContainer} ${styles.cycleTimeTeamView}`}
-      >
-        <TeamViewHeader dateSelect={true} />
-        {stats.map((stat, index) => (
+      <div className={styles.teamViewContainer}>
+        <TeamViewHeader
+          dateSelect={true}
+          onDateRangeChange={handleDateRangeChange}
+          options={[
+            { id: 'average', name: 'All Repositories' },
+            ...selectedRepositories.map(repo => ({
+              id: repo.id,
+              name: repo.name,
+            })),
+          ]}
+          onRepoSelect={handleRepoSelect}
+        />
+        <div className={styles.insight}>
           <StateCard
-            key={index}
-            icon={stat.icon}
-            value={stat.value}
-            label={stat.label}
+            icon={stats.icon}
+            value={stats.value}
+            label={stats.label}
             extendedStats={extendedStats}
           />
-        ))}
+        </div>
       </div>
       <MainBoard boardTitle="Cycle Time by PullRequest">
-        <CommonList headers={headers} data={cycleTimeData} />
+        <CommonList headers={HEADERS} data={filteredData} />
       </MainBoard>
+      {error && <div className={styles.error}>{error}</div>}
     </>
   );
 };
