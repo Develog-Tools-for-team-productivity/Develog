a908f3488a0849c4d8881a0ac291520fa92cf74e
Jeongsohee <toy3045@gmail.com>
2024-08-11 22:23:53 +0900

    ✨ Feat: #10 TeamView 사용자 정보 가져오기

--- src/components/header/TeamViewHeader.jsx
+++ src/components/header/TeamViewHeader.jsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
 import { useAtom } from 'jotai';
 import { selectedRepoAtom } from '../../stores/useStore';
+import { fetchData } from '../../utils/api';
 import DateRangePicker from '../select/DateRangePicker';
 import SelectBox from '../select/SelectBox';
 import styles from './header.module.css';
@@ -10,17 +12,46 @@ const TeamViewHeader = ({
   selectBox,
   options,
 }) => {
+  const [userData, setUserData] = useState({});
   const [selectedRepo, setSelectedRepo] = useAtom(selectedRepoAtom);
 
   const handleRepoSelect = repoId => {
     setSelectedRepo(repoId);
   };
 
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await fetchData(
+          `${import.meta.env.VITE_API_URL}/user-data`,
+          'GET',
+          null,
+          '사용자 데이터를 가져오는데 실패하였습니다'
+        );
+
+        setUserData(response);
+      } catch (error) {
+        console.error('데이터를 가져오지 못했습니다:', error);
+      }
+    };
+
+    getUserData();
+  }, []);
+
   return (
     <div className={styles.teamViewHeaderWrap}>
-      <p className={styles.teamName}>
-        <span>FE</span>FrontEnd Team
-      </p>
+      {userData.userInfo && (
+        <div className={styles.teamInfo}>
+          <span>
+            <img
+              src={userData.userInfo?.userImg}
+              alt="User"
+              className={styles.userImg}
+            />
+          </span>
+          <p>{userData.userInfo?.teamName}</p>
+        </div>
+      )}
       {dateSelect && <DateRangePicker onDateRangeChange={onDateRangeChange} />}
       {selectBox && (
         <SelectBox
